868e48c8a3b147ed43efe65258dff48db71d31b6
18064986 <867528315@qq.com>
2022-12-16 20:20:27 +0800

    单利

--- app/model/admin/system.sql.js
+++
@@ -1,9 +0,0 @@
-const db = require('../../config/db');
-let system = {
-    
-    'query':`SELECT id,webname,keyword,description,weburl,email,copyright,code FROM ys_system`,
-    'add':'INSERT INTO ys_system (webname,keyword,description,weburl,email,copyright,code) VALUES (?,?,?,?,?,?,?)',
-    'update':'UPDATE ys_system SET webname=?,keyword=?,description=?,weburl=?,email=?,copyright=?,code=? WHERE id=? LIMIT 1'
-};
-
-module.exports = system;
