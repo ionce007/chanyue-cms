32f805f13274617c1a82e8224c6f5403593d8dcc
18064986 <867528315@qq.com>
2023-02-06 21:50:25 +0800

    [新增功能] 实现微信小程序登录，后期开发会员模块。已完成获取unionid和openid和用户基本信息。

--- server/app/controller/weixin/index.js
+++ server/app/controller/weixin/index.js
@@ -1,12 +1,13 @@
 'use strict';
-const BaseController = require('./base');
-const dayjs = require('dayjs');
 const path = require('path');
+const dayjs = require('dayjs');
+const axios = require('axios');
+
+const BaseController = require('./base');
 const { success, fail } = require('../../extend/api.js');
 const { md5, setToken } = require('../../extend/helper.js');
-const WXBizDataCrypt = require('../../extend/WXBizDataCrypt.js');
 const { weixin } = require('../../config/config.js');
-const axios = require('axios');
+
 class WeiXinController extends BaseController {
   constructor(props) {
     super(props);
@@ -15,11 +16,11 @@ class WeiXinController extends BaseController {
 
   async login(req, res, next){
     try {
-        console.log(req.body)
         const {code,userInfo} = req.body;
         const {appid,secret} = weixin;
         const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`
         const result = await axios.default.get(url);
+        // 加密返回token,获取token解密然后通过openid或者unionid来查询
         res.json({
             code: 0,
             msg: 'success',
@@ -32,8 +33,6 @@ class WeiXinController extends BaseController {
         next(error);
     }
   }
-
-
 
 }
 
