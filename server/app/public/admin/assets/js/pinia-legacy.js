623be12b9f6029a9360a4b58d288d65c5e2b88ae
18064986 <867528315@qq.com>
2023-02-19 19:45:59 +0800

    [新功能] 优化service controller 改为静态方法

--- server/app/public/admin/assets/js/pinia-legacy.js
+++ server/app/public/admin/assets/js/pinia-legacy.js
@@ -1,6 +1,6 @@
-System.register(["./@vue-legacy.js"],(function(t,n){"use strict";var e,c,u;return{setters:[function(t){e=t.an,c=t.l,u=t.am}],execute:function(){t("c",(function(){var t=e(!0),n=t.run((function(){return c({})})),i=[],a=[],o=u({install:function(t){o._a=t,t.provide(r,o),t.config.globalProperties.$pinia=o,a.forEach((function(t){return i.push(t)})),a=[]},use:function(t){return this._a?i.push(t):a.push(t),this},_p:i,_a:null,_e:t,_s:new Map,state:n});return o}));var n,r=Symbol();
+System.register(["./@vue-legacy.js"],(function(t,n){"use strict";var e,c,u;return{setters:[function(t){e=t.ao,c=t.l,u=t.an}],execute:function(){t("c",(function(){var t=e(!0),n=t.run((function(){return c({})})),a=[],o=[],s=u({install:function(t){s._a=t,t.provide(i,s),t.config.globalProperties.$pinia=s,o.forEach((function(t){return a.push(t)})),o=[]},use:function(t){return this._a||r?a.push(t):o.push(t),this},_p:a,_a:null,_e:t,_s:new Map,state:n});return s}));var n,r=!1,i=Symbol();
 /*!
-        * pinia v2.0.28
-        * (c) 2022 Eduardo San Martin Morote
+        * pinia v2.0.30
+        * (c) 2023 Eduardo San Martin Morote
         * @license MIT
         */!function(t){t.direct="direct",t.patchObject="patch object",t.patchFunction="patch function"}(n||(n={}))}}}));
--- server/app/public/admin/assets/js/pinia.js
+++ server/app/public/admin/assets/js/pinia.js
@@ -1,5 +1,5 @@
-import{an as r,l,am as p}from"./@vue.js";var u=!1;/*!
-  * pinia v2.0.28
-  * (c) 2022 Eduardo San Martin Morote
+import{ao as r,l,an as p}from"./@vue.js";var u=!1;/*!
+  * pinia v2.0.30
+  * (c) 2023 Eduardo San Martin Morote
   * @license MIT
-  */const f=Symbol();var s;(function(t){t.direct="direct",t.patchObject="patch object",t.patchFunction="patch function"})(s||(s={}));function m(){const t=r(!0),i=t.run(()=>l({}));let c=[],n=[];const a=p({install(e){a._a=e,e.provide(f,a),e.config.globalProperties.$pinia=a,n.forEach(o=>c.push(o)),n=[]},use(e){return!this._a&&!u?n.push(e):c.push(e),this},_p:c,_a:null,_e:t,_s:new Map,state:i});return a}export{m as c};
+  */const f=Symbol();var s;(function(t){t.direct="direct",t.patchObject="patch object",t.patchFunction="patch function"})(s||(s={}));function _(){const t=r(!0),o=t.run(()=>l({}));let c=[],n=[];const a=p({install(e){a._a=e,e.provide(f,a),e.config.globalProperties.$pinia=a,n.forEach(i=>c.push(i)),n=[]},use(e){return!this._a&&!u?n.push(e):c.push(e),this},_p:c,_a:null,_e:t,_s:new Map,state:o});return a}export{_ as c};
