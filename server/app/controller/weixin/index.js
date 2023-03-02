74eeb26a9aa2aab43d39e0530ba1143d1e7b24b8
18064986 <867528315@qq.com>
2023-03-02 22:31:45 +0800

    [新功能] 更新依赖包，细节优化

--- server/app/controller/weixin/index.js
+++ server/app/controller/weixin/index.js
@@ -1,39 +1,38 @@
-'use strict';
-const path = require('path');
-const dayjs = require('dayjs');
-const axios = require('axios');
+"use strict";
+const path = require("path");
+const dayjs = require("dayjs");
+const axios = require("axios");
 
-const BaseController = require('./base');
-const { success, fail } = require('../../extend/api.js');
-const { md5, setToken } = require('../../extend/helper.js');
-const { weixin } = require('../../config/config.js');
+const BaseController = require("./base");
+const { success, fail } = require("../../extend/api.js");
+const { md5, setToken } = require("../../extend/helper.js");
+const { weixin } = require("../../config/config.js");
 
 class WeiXinController extends BaseController {
   constructor(props) {
     super(props);
-    this.model = 'weixin';
+    this.model = "weixin";
   }
 
-  static async login(req, res, next){
+  static async login(req, res, next) {
     try {
-        const {code,userInfo} = req.body;
-        const {appid,secret} = weixin;
-        const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`
-        const result = await axios.default.get(url);
-        // 加密返回token,获取token解密然后通过openid或者unionid来查询
-        res.json({
-            code: 0,
-            msg: 'success',
-            data:{
-                ...result.data,
-                userInfo
-            }
-        })
+      const { code, userInfo } = req.body;
+      const { appid, secret } = weixin;
+      const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`;
+      const result = await axios.default.get(url);
+      // 加密返回token,获取token解密然后通过openid或者unionid来查询
+      res.json({
+        code: 0,
+        msg: "success",
+        data: {
+          ...result.data,
+          userInfo,
+        },
+      });
     } catch (error) {
-        next(error);
+      next(error);
     }
   }
-
 }
 
-module.exports =  WeiXinController;
+module.exports = WeiXinController;
